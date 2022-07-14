9aaf6b0bdff5d7d3797114535c407049cafb1d94
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2022-07-14 17:53:10 +0000

    Added License.

--- server.c
+++ server.c
@@ -1,3 +1,12 @@
+// This file is part of file_sharer_server.
+// Copyright (C) 2022, github.com/CubedProgrammer, owner of said account.
+
+// file_sharer_server is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+// file_sharer_server is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along with file_sharer_server. If not, see <https://www.gnu.org/licenses/>. 
+
 #include<arpa/inet.h>
 #include<errno.h>
 #include<pthread.h>
